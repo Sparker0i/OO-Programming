6de80344c072568bda92233827d3707e6690bbb9
Aaditya Menon <AadityaMenon007@gmail.com>
2016-09-21 23:57:30 +0530

    Basic Input for Numerator and Denominator

--- Mini Project - Fraction Calculator/src/FractionCalculator.java
+++ Mini Project - Fraction Calculator/src/FractionCalculator.java
@@ -4,24 +4,25 @@ import java.awt.event.*;
 
 @SuppressWarnings("serial")
 public class FractionCalculator extends JFrame implements ActionListener {
-	JTextField tfield;
+	JTextField numInput, denInput;
 	double temp, temp1, result, a;
 	static double m1, m2;
 	int k = 1, x = 0, y = 0, z = 0;
 	char ch;
 	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, zero, clr, pow2, pow3, exp,
 			fac, plus, min, div, log, rec, mul, eq, addSub, dot, mr, mc, mp,
-			mm, sqrt, sin, cos, tan;					//Buttons
-	Container cont;										//Container
-	JPanel textPanel, buttonpanel;						//Panel/Layout
+			mm, sqrt, sin, cos, tan;								//Buttons
+	Container cont;													//Container
+	JPanel numPanel, denPanel, buttonpanel;							//Panel/Layout
 
 	FractionCalculator() {
-		cont = getContentPane();			//Layer To Hold Objects
-		cont.setLayout(new BorderLayout());		//BL arranges components in Regions E , W , N , S, Center. Each region has only one component
-		JPanel textpanel = new JPanel();		//Layout
-		tfield = new JTextField(25);
-		tfield.setHorizontalAlignment(SwingConstants.RIGHT);		//So that the Text when typed appears from right than the left
-		tfield.addKeyListener(new KeyAdapter() {					//To listen to the keyboard events
+		cont = getContentPane();									//Layer To Hold Objects
+		cont.setLayout(new BorderLayout());							//BL arranges components in Regions E , W , N , S, Center. Each region has only one component
+		numPanel = new JPanel();									//Layout
+		denPanel = new JPanel();
+		numInput = new JTextField(25);
+		numInput.setHorizontalAlignment(SwingConstants.RIGHT);		//So that the Text when typed appears from right than the left
+		numInput.addKeyListener(new KeyAdapter() {					//To listen to the keyboard events
 			public void keyTyped(KeyEvent keyevent) {				//To recognize which key was pressed
 				char c = keyevent.getKeyChar();						//Getting Key ASCII Value
 				if (c >= '0' && c <= '9') {
@@ -30,7 +31,19 @@ public class FractionCalculator extends JFrame implements ActionListener {
 				}
 			}
 		});
-		textpanel.add(tfield);										//To add the text field to the desired panel
+		numPanel.add(numInput);										//To add the text field to the desired panel
+		denInput = new JTextField(25);
+		denInput.setHorizontalAlignment(SwingConstants.RIGHT);		//So that the Text when typed appears from right than the left
+		denInput.addKeyListener(new KeyAdapter() {					//To listen to the keyboard events
+			public void keyTyped(KeyEvent keyevent) {				//To recognize which key was pressed
+				char c = keyevent.getKeyChar();						//Getting Key ASCII Value
+				if (c >= '0' && c <= '9') {
+				} else {
+					keyevent.consume();								//To Ignore if any other key was pressed
+				}
+			}
+		});
+		denPanel.add(denInput);										//To add the text field to the desired panel
 		buttonpanel = new JPanel();									//A New Panel For Buttons
 		buttonpanel.setLayout(new GridLayout(8, 4, 2, 2));			//To arrange buttons in a rectangular grid of width 4 and height 8, with a vertical gap of 2 and horizontal gap of 2
 		mr = new JButton("MR");										//Create A new Button with a label "MR"
@@ -145,8 +158,9 @@ public class FractionCalculator extends JFrame implements ActionListener {
 		clr = new JButton("AC");
 		buttonpanel.add(clr);
 		clr.addActionListener(this);
-		cont.add("Center", buttonpanel);							//Adds ButtonPanel to the Center Region
-		cont.add("North", textpanel);								//Add Input Panel to the North Region
+		cont.add("South", buttonpanel);								//Adds ButtonPanel to the South Region
+		cont.add("Center" , denPanel);								//Adds Denominator Input Panel to Center Region
+		cont.add("North", numPanel);								//Adds Numerator Panel to the North Region
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);				//What To Do When Close Has Been Clicked
 	}
 
@@ -155,281 +169,281 @@ public class FractionCalculator extends JFrame implements ActionListener {
 		//For Buttons 1 to 9 then 0
 		if (s.equals("1")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "1");
+				numInput.setText(numInput.getText() + "1");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "1");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "1");
 				z = 0;
 			}
 		}
 		if (s.equals("2")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "2");
+				numInput.setText(numInput.getText() + "2");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "2");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "2");
 				z = 0;
 			}
 		}
 		if (s.equals("3")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "3");
+				numInput.setText(numInput.getText() + "3");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "3");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "3");
 				z = 0;
 			}
 		}
 		if (s.equals("4")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "4");
+				numInput.setText(numInput.getText() + "4");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "4");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "4");
 				z = 0;
 			}
 		}
 		if (s.equals("5")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "5");
+				numInput.setText(numInput.getText() + "5");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "5");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "5");
 				z = 0;
 			}
 		}
 		if (s.equals("6")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "6");
+				numInput.setText(numInput.getText() + "6");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "6");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "6");
 				z = 0;
 			}
 		}
 		if (s.equals("7")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "7");
+				numInput.setText(numInput.getText() + "7");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "7");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "7");
 				z = 0;
 			}
 		}
 		if (s.equals("8")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "8");
+				numInput.setText(numInput.getText() + "8");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "8");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "8");
 				z = 0;
 			}
 		}
 		if (s.equals("9")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "9");
+				numInput.setText(numInput.getText() + "9");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "9");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "9");
 				z = 0;
 			}
 		}
 		if (s.equals("0")) {
 			if (z == 0) {
-				tfield.setText(tfield.getText() + "0");
+				numInput.setText(numInput.getText() + "0");
 			} else {
-				tfield.setText("");
-				tfield.setText(tfield.getText() + "0");
+				numInput.setText("");
+				numInput.setText(numInput.getText() + "0");
 				z = 0;
 			}
 		}
 		if (s.equals("AC")) {
-			tfield.setText("");										//Clears Input Field
+			numInput.setText("");										//Clears Input Field
 			x = 0;
 			y = 0;
 			z = 0;
 		}
 		if (s.equals("log")) {
-			if (tfield.getText().equals("")) {
-				tfield.setText("");									//If Input text is null, let it do nothing
+			if (numInput.getText().equals("")) {
+				numInput.setText("");									//If Input text is null, let it do nothing
 			} else {
-				a = Math.log(Double.parseDouble(tfield.getText()));	//Parses the String, gets its Log					
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);				//Sets it back to the text input
+				a = Math.log(Double.parseDouble(numInput.getText()));	//Parses the String, gets its Log					
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);				//Sets it back to the text input
 			}
 		}
 		if (s.equals("1/x")) {
-			if (tfield.getText().equals("")) {						//If input text is null, this snippet doesn't care less.
-				tfield.setText("");
+			if (numInput.getText().equals("")) {						//If input text is null, this snippet doesn't care less.
+				numInput.setText("");
 			} else {
-				a = 1 / Double.parseDouble(tfield.getText());		//Inverts the number after parsing it
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = 1 / Double.parseDouble(numInput.getText());		//Inverts the number after parsing it
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("Exp")) {
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.exp(Double.parseDouble(tfield.getText()));	//Calculates e ^ a
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);				//Sets it back to the text field.
+				a = Math.exp(Double.parseDouble(numInput.getText()));	//Calculates e ^ a
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);				//Sets it back to the text field.
 			}
 		}
 		if (s.equals("x^2")) {
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.pow(Double.parseDouble(tfield.getText()), 2);	//Parses the number, squares it
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.pow(Double.parseDouble(numInput.getText()), 2);	//Parses the number, squares it
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("x^3")) {
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.pow(Double.parseDouble(tfield.getText()), 3);	//Parses the number, cubes it
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.pow(Double.parseDouble(numInput.getText()), 3);	//Parses the number, cubes it
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("+/-")) {										//I've modified this one for my purposes, look at this only if you could care less.
-				a = Double.parseDouble(tfield.getText());
+				a = Double.parseDouble(numInput.getText());
 				a *= -1;
-				tfield.setText("" + a);
+				numInput.setText("" + a);
 		}
 		if (s.equals(".")) {
 			if (y == 0) {											//I don't get this completely, but I think it means if it hasn't already been done
-				tfield.setText(tfield.getText() + ".");
+				numInput.setText(numInput.getText() + ".");
 				y = 1;
 			} else {
-				tfield.setText(tfield.getText());
+				numInput.setText(numInput.getText());
 			}
 		}
 		if (s.equals("+")) {
-			if (tfield.getText().equals("")) {						//If null, sets the addend to be 0, and the operator as +
-				tfield.setText("");
+			if (numInput.getText().equals("")) {						//If null, sets the addend to be 0, and the operator as +
+				numInput.setText("");
 				temp = 0;
 				ch = '+';
 			} else {
-				temp = Double.parseDouble(tfield.getText());		//Parses the number input
-				tfield.setText("");									//Sets input box empty so as to enter the next number
+				temp = Double.parseDouble(numInput.getText());		//Parses the number input
+				numInput.setText("");									//Sets input box empty so as to enter the next number
 				ch = '+';											//Sets the operator to +
 				y = 0;												//Don't know the function of these
 				x = 0;
 			}
-			//tfield.requestFocus();
+			//numInput.requestFocus();
 		}
 		if (s.equals("-")) {
-			if (tfield.getText().equals("")) {						//Similar to +
-				tfield.setText("");
+			if (numInput.getText().equals("")) {						//Similar to +
+				numInput.setText("");
 				temp = 0;
 				ch = '-';
 			} else {
 				x = 0;
 				y = 0;
-				temp = Double.parseDouble(tfield.getText());
-				tfield.setText("");
+				temp = Double.parseDouble(numInput.getText());
+				numInput.setText("");
 				ch = '-';
 			}
-			tfield.requestFocus();
+			numInput.requestFocus();
 		}
 		if (s.equals("/")) {										//Similar to +
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 				temp = 1;
 				ch = '/';
 			} else {
 				x = 0;
 				y = 0;
-				temp = Double.parseDouble(tfield.getText());
+				temp = Double.parseDouble(numInput.getText());
 				ch = '/';
-				tfield.setText("");
+				numInput.setText("");
 			}
-			tfield.requestFocus();
+			numInput.requestFocus();
 		}
 		if (s.equals("*")) {										//Similar to +
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 				temp = 1;
 				ch = '*';
 			} else {
 				x = 0;
 				y = 0;
-				temp = Double.parseDouble(tfield.getText());
+				temp = Double.parseDouble(numInput.getText());
 				ch = '*';
-				tfield.setText("");
+				numInput.setText("");
 			}
-			tfield.requestFocus();
+			numInput.requestFocus();
 		}
 		if (s.equals("MC")) {										//Clears variable stored in memory
 			m1 = 0;
-			tfield.setText("");
+			numInput.setText("");
 		}
 		if (s.equals("MR")) {
-			tfield.setText("");
-			tfield.setText(tfield.getText() + m1);					//Retrieves variable stored in memory
+			numInput.setText("");
+			numInput.setText(numInput.getText() + m1);					//Retrieves variable stored in memory
 		}
 		if (s.equals("M+")) {
 			if (k == 1) {
-				m1 = Double.parseDouble(tfield.getText());			//No variable exists in memory, thus adds text input value to the memory
+				m1 = Double.parseDouble(numInput.getText());			//No variable exists in memory, thus adds text input value to the memory
 				k++;
 			} else {
-				m1 += Double.parseDouble(tfield.getText());			//Variable in memory exists, thus adds this value to the one in memory
-				tfield.setText("" + m1);							//Displays the new result in input field
+				m1 += Double.parseDouble(numInput.getText());			//Variable in memory exists, thus adds this value to the one in memory
+				numInput.setText("" + m1);							//Displays the new result in input field
 			}
 		}
 		if (s.equals("M-")) {
 			if (k == 1) {
-				m1 = Double.parseDouble(tfield.getText());			//No variable exists in memory, thus adds this variable to the memory
+				m1 = Double.parseDouble(numInput.getText());			//No variable exists in memory, thus adds this variable to the memory
 				k++;
 			} else {
-				m1 -= Double.parseDouble(tfield.getText());			//Variable already exists in memory, subtracts this value from the one stored in memory
-				tfield.setText("" + m1);
+				m1 -= Double.parseDouble(numInput.getText());			//Variable already exists in memory, subtracts this value from the one stored in memory
+				numInput.setText("" + m1);
 			}
 		}
 		if (s.equals("Sqrt")) {
-			if (tfield.getText().equals("")) {						//Doesnt care less if the text input is empty
-				tfield.setText("");
+			if (numInput.getText().equals("")) {						//Doesnt care less if the text input is empty
+				numInput.setText("");
 			} else {
-				a = Math.sqrt(Double.parseDouble(tfield.getText()));	//Else calculates the square root
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.sqrt(Double.parseDouble(numInput.getText()));	//Else calculates the square root
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("SIN")) {										//Calculates sin of the given angle (radians)
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.sin(Double.parseDouble(tfield.getText()));
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.sin(Double.parseDouble(numInput.getText()));
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("COS")) {										//Calculates cos of the given angle
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.cos(Double.parseDouble(tfield.getText()));
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.cos(Double.parseDouble(numInput.getText()));
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("TAN")) {										//Calculates tan of the given angle
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = Math.tan(Double.parseDouble(tfield.getText()));
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = Math.tan(Double.parseDouble(numInput.getText()));
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
 		if (s.equals("=")) {										//Does something when = button is pressed
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				temp1 = Double.parseDouble(tfield.getText());		//Gets the second variable, currently in the text input
+				temp1 = Double.parseDouble(numInput.getText());		//Gets the second variable, currently in the text input
 				switch (ch) {
 				case '+':
 					result = temp + temp1;
@@ -444,21 +458,21 @@ public class FractionCalculator extends JFrame implements ActionListener {
 					result = temp * temp1;
 					break;
 				}
-				tfield.setText("");
-				tfield.setText(tfield.getText() + result);			//Modifies text input to currently stored answer
+				numInput.setText("");
+				numInput.setText(numInput.getText() + result);			//Modifies text input to currently stored answer
 				z = 1;
 			}
 		}
 		if (s.equals("n!")) {										//Calculates n! and puts the value in the input box
-			if (tfield.getText().equals("")) {
-				tfield.setText("");
+			if (numInput.getText().equals("")) {
+				numInput.setText("");
 			} else {
-				a = fact(Double.parseDouble(tfield.getText()));
-				tfield.setText("");
-				tfield.setText(tfield.getText() + a);
+				a = fact(Double.parseDouble(numInput.getText()));
+				numInput.setText("");
+				numInput.setText(numInput.getText() + a);
 			}
 		}
-		tfield.requestFocus();
+		numInput.requestFocus();
 	}
 
 	double fact(double x) {
